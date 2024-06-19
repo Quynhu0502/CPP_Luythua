d0860887c6aded448f71810e4d60bbf59c90d915
SPC <SPC@20-LAB402>
2024-06-19 18:46:12 +0700

    Add project files.

---
+++ CPP_LUY THUA.cpp
@@ -0,0 +1,21 @@
+﻿// CPP_LUY THUA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so,mu,lt=1 ;
+	cout << "nhap so:";
+	cin >> so;
+	cout << "nhap mu:";
+	cin >>mu;
+	for (int i = 1; i <= mu; i++)
+	{
+		lt *= so;
+
+
+	}
+	cout << "lt"<<lt;
+	return 0;
+}
